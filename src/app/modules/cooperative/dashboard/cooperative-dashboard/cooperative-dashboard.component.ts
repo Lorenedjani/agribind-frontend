fb11df238c8d88d266f02e2ec222aa586855252f
Lorene Djani <paul.djani@saintjeaningenieur.org>
2025-10-24 23:35:18 +0100

    Initial commit: AGRIBIND-FRONTEND Angular project

---
+++ src/app/modules/cooperative/dashboard/cooperative-dashboard/cooperative-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cooperative-dashboard',
+  imports: [],
+  templateUrl: './cooperative-dashboard.component.html',
+  styleUrl: './cooperative-dashboard.component.scss'
+})
+export class CooperativeDashboardComponent {
+
+}
