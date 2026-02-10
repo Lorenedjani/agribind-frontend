f35956bb5f44afc9057ced3b064b399ae13ecc9b
Takam-Ismael <ismaelulrich121@gmail.com>
2026-02-10 02:52:55 +0100

    initialize ismael branch

--- source-code/platform-backend/user-management-service/target/generated-sources/annotations/cm/agribind/usermanagement/mapper/QueryMapperImpl.java
+++ source-code/platform-backend/user-management-service/target/generated-sources/annotations/cm/agribind/usermanagement/mapper/QueryMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-02-10T02:28:52+0100",
+    date = "2026-02-10T02:43:18+0100",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.45.0.v20260128-0750, environment: Java 21.0.9 (Eclipse Adoptium)"
 )
 @Component
